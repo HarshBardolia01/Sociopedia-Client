8006ff3b3f0e7788798172003e5384b5c1cff196
HarshBardolia01 <harshbardolia02@gmail.com>
2023-05-01 19:52:53 +0530

    updated fetching URL

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://social-media-server1-4ouu.onrender.com${userId}/friends`,
+      `https://social-media-server1-4ouu.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
